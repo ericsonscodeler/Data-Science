9ba4c36603fab702c296f48cad3bc657d53500f2
Ericson Scodeler <ericson.br@hotmail.com>
2020-03-09 14:22:48 -0300

    usando matplotlib python para plotar grafico

--- Exercicios/Exercicio3/Exercicio3.py
+++ Exercicios/Exercicio3/Exercicio3.py
@@ -16,14 +16,14 @@ class NeuralNetwork():
 
         
     def __init__(self,file=None):
+        
      
         self.df = pd.read_csv(file)
 
-        
-        self.df.columns = ['Produto','Alcool', 'Acido malico', 'cinza','Alcalinidade das cinzas',
+        self.df.columns = ['Produtor','Alcool', 'Acido malico', 'cinza','Alcalinidade das cinzas',
         'Magnesio', 'Fenois totais', 'Flavonoides','Fenois não flavonoides ', 'Proantocianinas',
         'Intensidade da cor', 'Matiz','OD315 de vinhos diluídos ', 'Prolina']
-        self.df.columns
+        ##self.df.columns
         print("Menor valor de cada coluna")
         print(self.df.min())
         print("###########################")
@@ -44,41 +44,87 @@ class NeuralNetwork():
         print(self.df.head()) #apresenta as 5 primeiras linhas
         print("dimensao do arquivo csv")
         print(self.df.shape) # tupla de dimensao
+        
+        self.menu()
+        
 
 
         ####################################
             ###GRAFICO
         ####################################
+    def grafico(self):
 
         y = np.linspace(80,160,20)
         x = np.linspace(11,15,20)
         plt.xlabel(u'Alcool')
         plt.ylabel(u'Magnesio')
         self.df.plot(kind='scatter',x='Alcool',y='Magnesio')
-        
 
 
-
-
-        #plt.plot(x,y)
+        plt.legend()
+        plt.plot(x,y)
         plt.show(True)
 
 
+    def previsao_rede_neural(self):
+        
+        produtor1 = self.df.loc[(self.df['Produtor']) == 1]
+        produtor2 = self.df.loc[(self.df['Produtor']) == 2]
+        produtor3 = self.df.loc[(self.df['Produtor']) == 3]
+
+        produtor1_x = produtor1.drop('Produtor',axis=1)
+        produtor1_y = produtor1['Produtor']
+
+        produtor2_x = produtor2.drop('Produtor',axis=1)
+        produtor2_y = produtor2['Produtor']
+
+        produtor3_x = produtor3.drop('Produtor',axis=1)
+        produtor3_y = produtor3['Produtor']
+
+        treino_produtor1_x,teste_produtor1_x,treino_produtor1_y,teste_produtor1_y = train_test_split(produtor1_x,produtor1_y,test_size=0.25,stratify=produtor1_y)
+        treino_produtor2_x,teste_produtor2_x,treino_produtor2_y,teste_produtor2_y = train_test_split(produtor2_x,produtor2_y,test_size=0.25,stratify=produtor2_y)
+        treino_produtor3_x,teste_produtor3_x,treino_produtor3_y,teste_produtor3_y = train_test_split(produtor3_x,produtor3_y,test_size=0.25,stratify=produtor3_y)
+
+        self.rede = MLPClassifier()
+        self.rede.fit(treino_produtor1_x,treino_produtor1_y)
+        self.rede.fit(treino_produtor2_x,treino_produtor2_y)
+        self.rede.fit(treino_produtor3_x,treino_produtor3_y)
+
+        produtor1_previsao_y = self.rede.predict(teste_produtor1_x)
+        produtor2_previsao_y = self.rede.predict(teste_produtor2_x)
+        produtor3_previsao_y = self.rede.predict(teste_produtor3_x)
+
+        print("######## PRODUTOR 1 ########")
+        print(accuracy_score(teste_produtor1_y,produtor1_previsao_y)* 100)
+        print(confusion_matrix(teste_produtor1_y,produtor1_previsao_y))
+
+        print("######## PRODUTOR 2 ########")
+        print(accuracy_score(teste_produtor2_y,produtor2_previsao_y)* 100)
+        print(confusion_matrix(teste_produtor2_y,produtor2_previsao_y))
+
+        print("######## PRODUTOR 3 ########")
+        print(accuracy_score(teste_produtor3_y,produtor3_previsao_y)* 100)
+        print(confusion_matrix(teste_produtor3_y,produtor3_previsao_y))
+
         
 
-        
 
-    # def menu(self):
-    #     while(True):
-    #        os.system("cls")
-    #        print("#######REDE NEURAL PARA PREVISAO DE PRODUTORES DE VINHO ########")
-    #        print("1 - Exibir dados originais e estatísticas")
-    #        print("2 - Imprimir grafico de Teor Alcoolico / Magnesio / Fenois por produtor")         
-    #        print("3 - Exibir previsao da Rede Neural")
-    #        choice =input()
+    def menu(self):
+        while(True):
+           os.system("cls")
+           print("#######REDE NEURAL PARA PREVISAO DE PRODUTORES DE VINHO ########")
+           print("1 - Exibir dados originais e estatísticas")
+           print("2 - Imprimir grafico de Teor Alcoolico / Magnesio / Fenois por produtor")         
+           print("3 - Exibir previsao da Rede Neural")
+           choice = input()
 
-    #        if choice == '1':
-    #            self.__init__()
+        #    if choice == '1':
+        #        self.__init__()
+        #        break
+        #     else choice == '2':
+        #         self.grafico()
+
+
 
 
 rede = NeuralNetwork("wine.csv")
